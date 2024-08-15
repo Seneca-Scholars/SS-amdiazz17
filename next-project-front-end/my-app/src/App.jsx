30ae4980b1144c7adc48fe453c8860c369f7ff44
Ava Diaz <amdiazz17@Avas-MacBook-Pro.local>
2024-08-15 13:06:37 -0500

    react app

---
+++ next-project-front-end/my-app/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+  <div>
+    <table>
+      <theader>
+        API DATA TABLE
+      </theader>
+      <tbody>
+        <th>Name</th>
+        <tr>data name 1</tr>
+        <tr>data name 2</tr>
+      </tbody>
+    </table>
+  </div>
+  );
+}
+
+
+export default App;
