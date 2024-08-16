38e7f06b3c56282b69530412c637bc1a67471494
Ava Diaz <amdiazz17@Avas-MacBook-Pro.local>
2024-08-16 11:39:50 -0500

    changed directory

---
+++ my-ap/src/App.jsx
@@ -0,0 +1,46 @@
+import './App.css';
+function App() {
+
+
+
+
+  
+  return (
+    <div className="app">
+      <header className="app-header">
+        Next Project
+      </header>
+      <body>
+      <div className="main">
+        <table id="">
+          <thead>
+            <tr>
+              <th>Numbers</th>
+              <th>Letters</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1234</td>
+              <td>abcd</td>
+            </tr>
+            <tr>
+              <td>1234</td>
+              <td>abcd</td>
+            </tr>
+            <tr>
+              <td>1234</td>
+              <td>abcd</td>
+            </tr>
+            <tr>
+              <td>1234</td>
+              <td>abcd</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      </body>
+    </div>
+  );
+}
+export default App;
