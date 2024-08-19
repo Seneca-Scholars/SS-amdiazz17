cfdcb07c8f82a8a819e170e2d583c44e2617caff
Ava Diaz <amdiazz17@Avas-MacBook-Pro.local>
2024-08-19 14:31:37 -0500

    updated project

--- front-end-project/src/App.jsx
+++ front-end-project/src/App.jsx
@@ -1,65 +1,58 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  function loadData(){
-    const dataUrl = "https://jsonplaceholder.typicode.com/users"
-    fetch(dataUrl)
-    .then(handleResponse)
-    .then(handleUrlData);
-  }
-  function handleResponse(response){
-    return response.json();
-  }
+  //set to an empty strinng before we fetch the data
+  const [data, setData] = useState([]);
+    useEffect(() => {
+      //fetches the data
+      fetch("https://jsonplaceholder.typicode.com/users")
+          .then(response => {
+            //if the request was unsuccuessful it throws an error
+              if (!response.ok) {
+                  throw new Error('Network response was not ok');
+              }
+              //turns the data into JSON and prepares it for the next step in the chain
+              return response.json();
+          })
+          //updates the state from eariler to the data that was fetched and parsed into JSON
+          .then(data => {
+            setData(data);
+          })
+          //console logs an error if onne occurs
+          .catch(error => {
+              console.error('Error fetching data:', error);
+          });
 
-  function load(){
-    loadData();
-  }
-
-  window.onload = load;
-
-
-
-
-
-
+  }, []);
 
 
   return (
-    <div className="app">
-      <header className="app-header">
-        Next Project
-      </header>
-      <body>
-      <div className="main">
-        <table id="">
-          <thead>
-            <tr>
-              <th>Numbers</th>
-              <th>Letters</th>
+    <div className="whole-table">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* .map iterates over all the objects in the array to generate table rows */}
+          {data.map((user, index) => (
+            <tr
+              key={user.id}
+              className={index % 2 === 0 ? 'even-row' : 'odd-row'}
+            >
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.phone}</td>
             </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1234</td>
-              <td>abcd</td>
-            </tr>
-            <tr>
-              <td>1234</td>
-              <td>abcd</td>
-            </tr>
-            <tr>
-              <td>1234</td>
-              <td>abcd</td>
-            </tr>
-            <tr>
-              <td>1234</td>
-              <td>abcd</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      </body>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
+
 export default App;
